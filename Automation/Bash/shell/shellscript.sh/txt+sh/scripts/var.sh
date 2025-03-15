c886d312c869274e492ebe11f4bf49815d1db407
rithishsamm <rithisam0526@gmail.com>
2025-03-15 10:58:37 +0530

    vault backup: 2025-03-15 10:58:36
    Affected files:
    Too many files to list

--- Automation/Bash/shell/shellscript.sh/txt+sh/scripts/var.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#this script is about variable, a value/datatype that holds within a string, namely VAR.
-#you can compose or call in in a script as per your convenience or relevant to the logic applies
-
-MESSAGE="Hello World" 
-NAME=" RITHISH SAM"
-
-echo $MESSAGE, Myself $NAME
